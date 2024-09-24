7b4422dde5f0e8ba4b8292398d9cf09ba6be2618
Mohammad Shafaque Arif <97733931+mohdshafaquearif@users.noreply.github.com>
2024-09-24 17:47:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
 let string = "";
-let arr = Array.from(buttons);
+let arr = Array.from(button);
 arr.forEach(button => {
     button.addEventListener('click', (e) =>{
         if(e.target.innerHTML == '='){
